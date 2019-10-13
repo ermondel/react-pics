2be39d7947fe6f2ccffc2d632fcdfd454e8d476e
Ermondel <ermondel@gmail.com>
2019-10-13 16:57:56 +0300

    feat: environment variable for unsplash api key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID '
+    Authorization: 'Client-ID ' + process.env.REACT_APP_UNSPLASH_API_KEY
   }
 });
